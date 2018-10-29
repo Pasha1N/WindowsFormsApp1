d30f411989481dffe052b0e5b30b1d060beb9170
Паша <@DESKTOP-3C2NDOK>
2018-10-29 13:45:30 +0300

    Firs Commit.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,20 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Speech.Recognition;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        static Label l;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Confidence > 0.82) l.Text = e.Result.Text;
+        }
+     
+        private void Form1_Shown_1(object sender, EventArgs e)
+        {
+            l = label1;
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+            Choices numbers = new Choices();
+            numbers.Add(new string[] { "один", "два", "три", "привет", "пять" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = ci;
+            gb.Append(numbers);
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            sre.RecognizeAsync(RecognizeMode.Multiple);
         }
     }
 }
